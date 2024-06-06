179c7069d9a053fea603de5ccd3a1ba292f1839d
Vinicius Santos <vinisantoosk@gmail.com>
2024-06-06 19:43:06 -0300

    feat: adicionando seção faq e footer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
 
     for (let i = 0; i < buttons.length; i++) {
@@ -16,8 +17,20 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', toggleAnswer)
+        
+    }
     
 })
+function toggleAnswer(elemento){
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle( classe);
+}
+
 function removeBotoesAtivos(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i = 0; i < buttons.length; i++) {
